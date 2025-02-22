9592c4e6588f8ebc3f4640f99c52773594f452a5
Pradeep Kumar Maurya <62380961+pradeep-kumar-maurya@users.noreply.github.com>
2025-02-22 21:10:35 +0530

    Time: 3 ms (91.21%) | Memory: 21.1 MB (89.91%) - LeetSync

--- 347-top-k-frequent-elements/top-k-frequent-elements.py
+++ 347-top-k-frequent-elements/top-k-frequent-elements.py
@@ -1,36 +1,44 @@
 class Solution:
     def topKFrequent(self, nums: List[int], k: int) -> List[int]:
-        map1 = {}
-        map2 = {}
-        max_count = 0
+        '''
+        1st: Maintain a frequency dict to record the frequency of all the numbers in nums array.
+        2nd: Also, maintain the greatest frequency seen among all the frequencies.
+        3rd: Reverse the frequency dict i.e. for multiple same frequencies, the value will be an array of all the numbers that has
+             that frequency.
+        4th: Use a while loop and keep on decrementing the greatest frequency by 1 and keep on searching it within the reversed dict.
+             As soon as we get top k elements, return the ans array.
+        '''
+        map = {}
+        reverse_map = {}
+        greatest_frequency = 0
         temp_count = 0
         ans = []
 
         for num in nums:
-            if map1.get(num) is None:
-                map1[num] = 0
+            if map.get(num) is None:
+                map[num] = 0
             else:
-                map1[num] += 1
+                map[num] += 1
             
-            if map1[num] > max_count:
-                max_count = map1[num]
+            if map[num] > greatest_frequency:  # finding the greatest frequency
+                greatest_frequency = map[num]
 
-        for key, value in map1.items():
-            if map2.get(value) is None:
-                map2[value] = [key]
+        for key, value in map.items():  # reversing the map dict
+            if reverse_map.get(value) is None:
+                reverse_map[value] = [key]
             else:
-                map2[value].append(key)
+                reverse_map[value].append(key)
 
-        while max_count >= 0:
-            if map2.get(max_count):
-                data = map2.get(max_count)
+        while greatest_frequency >= 0:  # decrementing greatest_frequency by 1
+            if reverse_map.get(greatest_frequency):
+                data = reverse_map.get(greatest_frequency)
 
                 for num in data:
                     ans.append(num)
                     temp_count += 1
-                    if temp_count == k:
+                    if temp_count == k:  # when k == temp_count, return ans array
                         return ans
     
-            max_count -= 1
+            greatest_frequency -= 1
         
         return ans
