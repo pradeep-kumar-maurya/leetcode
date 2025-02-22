71563e207a56fce51edc3dcbb748d90a0f5803a8
Pradeep Kumar Maurya <62380961+pradeep-kumar-maurya@users.noreply.github.com>
2025-02-22 20:51:07 +0530

    Time: 3 ms (91.21%) | Memory: 21.2 MB (62.16%) - LeetSync

--- 347-top-k-frequent-elements/top-k-frequent-elements.py
+++ 347-top-k-frequent-elements/top-k-frequent-elements.py
@@ -1,48 +1,36 @@
 class Solution:
     def topKFrequent(self, nums: List[int], k: int) -> List[int]:
-        """
-        The idea is to use two hashmaps. One hashmap will be used to maintain the frequency of the elements from the nums
-        array and the other hashmap will be the reverse of the 1st hashmap and this 2nd hashmap will have values as a list
-        because frequencies of different elements could be same and while reversing the 1st hashmap, multiple items with
-        same values would come under one key in the reversed hashmap. Also, mathematically k would never be too large and
-        after creating the reversed hashmap our job is keep on decrementing the max_count until ans array length is equal to k.
-        """
-        max_count = 0  # frequency of the element that appeared max in the nums array
-        freq_dict = {}  # 1st hashmap to store the frequencies of the elements from nums array
-        rev_dict = {}  # 2nd hashmap i.e. reverse of the 1st hashmap but values as a list
+        map1 = {}
+        map2 = {}
+        max_count = 0
+        temp_count = 0
         ans = []
 
         for num in nums:
-            if freq_dict.get(num) is None:
-                freq_dict[num] = 1
-                temp_count = 1
+            if map1.get(num) is None:
+                map1[num] = 0
             else:
-                freq_dict[num] += 1
-                temp_count = freq_dict[num]
-            if temp_count > max_count:  # maintain max_count
-                max_count = temp_count
+                map1[num] += 1
+            
+            if map1[num] > max_count:
+                max_count = map1[num]
 
-        if len(freq_dict) == k:  # if len(freq_dict) == k just simply return 1st hashmap keys
-            return freq_dict.keys()
-
-        # Now, we need to reverse the 1st hashmap because we will find the most frequent k elements by decrementing the
-        # max_count and that should be O(1) operation. While reversing, key will become value and value will become key
-        # but value this time has to be a list because multiple elements could have same frequency in the 1st hashmap.
-        for key, val in freq_dict.items():
-            if rev_dict.get(val) is None:
-                rev_dict[val] = [key]  # maintain values as a key
+        for key, value in map1.items():
+            if map2.get(value) is None:
+                map2[value] = [key]
             else:
-                rev_dict[val].append(key)  # append elements under the same key if their frequencies are same
+                map2[value].append(key)
 
-        # Now, we need to just iterate until len of ans array is equal to k
-        while True:
-            # We will start from max_count and keep on decrementing it by 1 because this way we will be getting our most
-            # frequent top k elements. Also, order does not matter here.
-            data = rev_dict.get(max_count)
-            if data:  # if data is not None then just extend the data list to ans array
-                ans.extend(data)
-            if len(ans) == k:  # if len(ans) == k, we have our top k elements, therefore, just break
-                break
-            max_count -= 1  # keep on decrementing k by 1
+        while max_count >= 0:
+            if map2.get(max_count):
+                data = map2.get(max_count)
 
+                for num in data:
+                    ans.append(num)
+                    temp_count += 1
+                    if temp_count == k:
+                        return ans
+    
+            max_count -= 1
+        
         return ans
