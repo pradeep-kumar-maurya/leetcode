5437bd146d6ec5c07efa78f39a366f1c47cdec8e
Pradeep Kumar Maurya <62380961+pradeep-kumar-maurya@users.noreply.github.com>
2025-02-22 20:14:22 +0530

    Time: 123 ms (69.72%) | Memory: 58 MB (21.72%) - LeetSync

--- 380-insert-delete-getrandom-o1/insert-delete-getrandom-o1.py
+++ 380-insert-delete-getrandom-o1/insert-delete-getrandom-o1.py
@@ -1,8 +1,8 @@
 class RandomizedSet:
     def __init__(self):
-        self.map = {}
-        self.values = []
-        self.index = -1
+        self.map = {}  # store element as key and their index as value
+        self.values = []  # use getRandom() on value array for O(1)
+        self.index = -1  # to maintain the index of the inserted element in the map
 
     def getRandom(self):
         return random.choice(self.values) if self.values else None
@@ -23,16 +23,19 @@ class RandomizedSet:
             i = self.map[val]
             j = len(self.values) - 1
 
+            # moving element at index i to the last index and then popping it from the values array
             temp = self.values[j]
             self.values[j] = self.values[i]
             self.values[i] = temp
 
-            self.values.pop()
-            self.index -= 1
-            self.map.pop(val)
+            self.values.pop()  # pop the element from the values array : O(1)
+            self.index -= 1  # reduce index by 1 every time
+            self.map.pop(val)  # pop the element from the map too
 
+            # "i != j" is important while removing the last element from the values array
             if len(self.values) > 0 and i != j:
                 self.map[temp] = i
+
             return True
 
 # Your RandomizedSet object will be instantiated and called as such:
