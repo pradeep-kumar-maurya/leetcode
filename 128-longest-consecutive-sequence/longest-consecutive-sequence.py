f9f85a78916e93068d2bb058faf42e390ace206d
Pradeep Kumar Maurya <62380961+pradeep-kumar-maurya@users.noreply.github.com>
2025-03-30 15:55:04 +0530

    Time: 58 ms (37.08%) | Memory: 34.3 MB (32.19%) - LeetSync

--- 128-longest-consecutive-sequence/longest-consecutive-sequence.py
+++ 128-longest-consecutive-sequence/longest-consecutive-sequence.py
@@ -27,27 +27,38 @@ class Solution:
 
         # OR
 
-        # freq_set = set()
+        '''
+        The idea is to create two sets. One set will store the elements from the arr and the other set will store the consecutive
+        elements from the arr. If element is already present in the consecutve set then skip that element. If not, first go through
+        all the elements left to the current element and check if they are all there in the arr set and then go through all the
+        elements right to the current element and check if they are all there in the arr set. All these left and right elments
+        will be added in the consecutive set.
+        '''
+        # arr_set = set()
         # consecutive_set = set()
         # max_size = 0
 
-        # for num in nums:
-        #     freq_set.add(num)
+        # for num in arr:
+        #     arr_set.add(num)  # create the arr set
 
-        # for num in nums:
+        # for num in arr:
 
         #     temp1 = num
         #     temp2 = num
         #     size = 0
 
-        #     while ((temp1 - 1) in freq_set) and ((temp1 - 1) not in consecutive_set):
+        #     # go through all the elements left to the current num and all the left elements must be present in the array set but
+        #     # not present in the consecutive_set
+        #     while ((temp1 - 1) in arr_set) and ((temp1 - 1) not in consecutive_set):
         #         size += 1
-        #         consecutive_set.add(temp1 - 1)
+        #         consecutive_set.add(temp1 - 1)  # keep on adding elements to the consecutive set
         #         temp1 -= 1
 
-        #     while ((temp2 + 1) in freq_set) and ((temp2 + 1) not in consecutive_set):
+        #     # go through all the elements right to the current num and all the right elements must be present in the array set but
+        #     # not present in the consecutive_set
+        #     while ((temp2 + 1) in arr_set) and ((temp2 + 1) not in consecutive_set):
         #         size += 1
-        #         consecutive_set.add(temp2 + 1)
+        #         consecutive_set.add(temp2 + 1)  # keep on adding elements to the consecutive set
         #         temp2 += 1
             
         #     if size + 1 > max_size:
