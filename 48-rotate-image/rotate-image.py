27ab03746dbda03ece1b2b73198b965447dd0ece
Pradeep Kumar Maurya <62380961+pradeep-kumar-maurya@users.noreply.github.com>
2025-03-30 17:41:43 +0530

    Time: 0 ms (100.00%) | Memory: 17.7 MB (67.37%) - LeetSync

--- 48-rotate-image/rotate-image.py
+++ 48-rotate-image/rotate-image.py
@@ -1,28 +1,26 @@
 class Solution:
     def rotate(self, matrix: List[List[int]]) -> None:
-        """
-        The idea is to first swap the elements by just iterating over the upper triangular matrix or the lowe triangular
-        matrix. We need to swap matrix[i][j] with matrix of [j][i] where i != j. This is nothing but the transpose of the
-        matrix. Now, we just need to reverse the column values per row, and it will give us the desired matrix i.e.
-        rotated by 90 degrees clockwise.
-        """
-        rows = len(matrix)
-        cols = len(matrix[0])
+        for i, row in enumerate(matrix):
 
-        # iterating over the upper triangular matrix
-        for i in range(0, rows, 1):
-            for j in range(i + 1, cols, 1):  # iterate over the columns where j starts from i+1
-                # swap the values
-                temp = matrix[i][j]
-                matrix[i][j] = matrix[j][i]
-                matrix[j][i] = temp
+            j = i + 1
 
-        # iterate row wise and reverse the row values
-        for row in range(0, rows, 1):
-            i, j = 0, cols - 1  # two pointers to reverse the row values
-            while i < j:
-                temp = matrix[row][j]
-                matrix[row][j] = matrix[row][i]
-                matrix[row][i] = temp
+            while j < len(row):
+
+                temp = matrix[j][i]
+                matrix[j][i] = matrix[i][j]
+                matrix[i][j] = temp
+
+                j += 1
+
+        for row in matrix:
+            i = 0
+            j = len(row) - 1
+
+            while i <= j:
+                temp = row[j]
+                row[j] = row[i]
+                row[i] = temp
                 i += 1
                 j -= 1
+
+        return matrix
