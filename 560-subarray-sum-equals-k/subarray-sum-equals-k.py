777287d5267fbe1c541fa685b7fe86ec46203630
Pradeep Kumar Maurya <62380961+pradeep-kumar-maurya@users.noreply.github.com>
2025-03-31 13:59:38 +0530

    Time: 29 ms (71.35%) | Memory: 20.3 MB (70.57%) - LeetSync

--- 560-subarray-sum-equals-k/subarray-sum-equals-k.py
+++ 560-subarray-sum-equals-k/subarray-sum-equals-k.py
@@ -1,7 +1,12 @@
 class Solution:
     def subarraySum(self, nums: List[int], k: int) -> int:
-        freq_dict = {0: 1}  # dict to maintain freq of prefix sums
+        '''
+        We should not be building the freq_dict ahead, instead, it should be built one by one by calculating the prefix sum in
+        each step and then checking if (prefix sum - k) exits in the freq_dict or not. And later, if prefix sum does not exit in
+        the freq_dict then add the prefix sum with frequency as 1 or if it exits then increment the frequency by 1.
+        '''
         prefix_sum = 0
+        freq_dict = {0: 1}  # 0 will be used when the prefix sum is equals to k
         count = 0
 
         for num in nums:
