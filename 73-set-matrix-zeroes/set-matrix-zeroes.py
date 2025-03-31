488fba2547151f8b48ea1ee529e9f6592296352b
Pradeep Kumar Maurya <62380961+pradeep-kumar-maurya@users.noreply.github.com>
2025-04-01 00:18:45 +0530

    Time: 3 ms (71.08%) | Memory: 18.5 MB (39.08%) - LeetSync

--- 73-set-matrix-zeroes/set-matrix-zeroes.py
+++ 73-set-matrix-zeroes/set-matrix-zeroes.py
@@ -1,35 +1,62 @@
 class Solution:
     def setZeroes(self, matrix: List[List[int]]) -> None:
+
+        # Below one takes extra space
+        rows_with_zero = set()
+        cols_with_zero = set()
         rows = len(matrix)
         cols = len(matrix[0])
 
         for i in range(rows):
+
             for j in range(cols):
 
                 if matrix[i][j] == 0:
-                    if isinstance(matrix[0][j], str):
-                        matrix[0][j] = '0'
-                    else:
-                        matrix[0][j] = 0
-                    matrix[i][0] = str(matrix[i][0])
+                    rows_with_zero.add(i)
+                    cols_with_zero.add(j)
 
         for i in range(rows):
+
             for j in range(cols):
 
-                if matrix[0][j] in ('0', 0):
-                    if isinstance(matrix[i][0], str):
-                        matrix[i][j] = "0"
-                    else:
-                        matrix[i][j] = 0
-
-        for i in range(rows):
-            for j in range(cols):
-
-                if isinstance(matrix[i][0], str):
-                    matrix[i][j] = '0'
-
-        for i in range(rows):
-            for j in range(cols):
-
-                if isinstance(matrix[i][j], str):
+                if i in rows_with_zero:
                     matrix[i][j] = 0
+                if j in cols_with_zero:
+                    matrix[i][j] = 0
+
+
+        # Below one takes no extra space
+
+        # rows = len(matrix)
+        # cols = len(matrix[0])
+
+        # for i in range(rows):
+        #     for j in range(cols):
+
+        #         if matrix[i][j] == 0:
+        #             if isinstance(matrix[0][j], str):
+        #                 matrix[0][j] = '0'
+        #             else:
+        #                 matrix[0][j] = 0
+        #             matrix[i][0] = str(matrix[i][0])
+
+        # for i in range(rows):
+        #     for j in range(cols):
+
+        #         if matrix[0][j] in ('0', 0):
+        #             if isinstance(matrix[i][0], str):
+        #                 matrix[i][j] = "0"
+        #             else:
+        #                 matrix[i][j] = 0
+
+        # for i in range(rows):
+        #     for j in range(cols):
+
+        #         if isinstance(matrix[i][0], str):
+        #             matrix[i][j] = '0'
+
+        # for i in range(rows):
+        #     for j in range(cols):
+
+        #         if isinstance(matrix[i][j], str):
+        #             matrix[i][j] = 0
