900f576fe2e6f047f3834944aeb9fbf8b78763b5
Pradeep Kumar Maurya <62380961+pradeep-kumar-maurya@users.noreply.github.com>
2025-04-06 00:36:28 +0530

    Time: 0 ms (100.00%) | Memory: 17.9 MB (14.51%) - LeetSync

--- 31-next-permutation/next-permutation.py
+++ 31-next-permutation/next-permutation.py
@@ -1,23 +1,10 @@
 class Solution:
     def nextPermutation(self, arr: List[int]) -> None:
+
         i = len(arr) - 1
         break_index = None
-        max = arr[-1]
-        max_ele_index = -1
-        diff = float('inf')
-        second_max_ele_index = -1
 
         while i >= 1:
-
-            if arr[i] > max:
-                max = arr[i]
-                max_ele_index = i
-
-            temp_diff = max - arr[i]
-
-            if temp_diff < diff and arr[i] != max:
-                diff = temp_diff
-                second_max_ele_index = i
 
             if arr[i] > arr[i - 1]:
                 break_index = i - 1
@@ -38,7 +25,16 @@ class Solution:
             arr[index] = arr[break_index]
             arr[break_index] = temp
             
-            arr[break_index + 1: ] = sorted(arr[break_index + 1: ])
+            i = break_index + 1
+            j = len(arr) - 1
+
+            while i < j:
+                temp = arr[j]
+                arr[j] = arr[i]
+                arr[i] = temp
+
+                i += 1
+                j -= 1        
 
 
         else:  # case where everything was sorted in descending order
