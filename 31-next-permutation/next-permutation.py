cbbd6a96353ad05222344b09b1e9879ab8fe1c1f
Pradeep Kumar Maurya <62380961+pradeep-kumar-maurya@users.noreply.github.com>
2025-04-06 00:56:46 +0530

    Time: 0 ms (100.00%) | Memory: 17.6 MB (85.93%) - LeetSync

--- 31-next-permutation/next-permutation.py
+++ 31-next-permutation/next-permutation.py
@@ -4,22 +4,31 @@ class Solution:
         i = len(arr) - 1
         break_index = None
 
+        # We need to find the index of the element that breaks the descending order condition in the array from right to left.
         while i >= 1:
 
             if arr[i] > arr[i - 1]:
                 break_index = i - 1
-                break
+                break  # we can break as soon as we find the breaking index
 
             i -= 1
 
         if break_index is not None:
 
             index = -1
+
+            '''
+            We need to iterate from right to left and find the first element which is greater than arr[break_index].
+            We do this because right part of the break_index will always be sorted in the descending order and therefore,
+            we just need to find the first element from right which is > than arr[break_index] and swap both of them and
+            then reverse the arr from (break_index + 1 till N - 1). Reversing the arr from (break_index + 1 till N - 1)
+            will basically sort the array and hence we will get the next lexicographically greater permutation.
+            '''
             for i in range(len(arr) - 1, break_index, -1):
 
                 if arr[break_index] < arr[i]:
                     index = i
-                    break
+                    break  # break as soon as arr[i] is > than arr[break_index]
 
             temp = arr[index]
             arr[index] = arr[break_index]
@@ -29,11 +38,14 @@ class Solution:
             j = len(arr) - 1       
 
 
-        else:  # case where everything was sorted in descending order
+        # case where everything was sorted in descending order, i will be 0 and j will be N - 1
+        else:
             i = 0
             j = len(arr) - 1
 
-        while i < j:  # swap all the elements, basically sort the array in the ascending order
+        # Swap all the elements, basically, sort the array in the ascending order from index i till j.
+        # This swapping will be performed in both the cases.
+        while i < j:
             temp = arr[j]
             arr[j] = arr[i]
             arr[i] = temp
